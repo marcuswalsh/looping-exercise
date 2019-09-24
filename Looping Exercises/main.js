422e32e0cb2dd57d20a854b6d121a89b8ee3b31e
marcuswalsh <34656505+marcuswalsh@users.noreply.github.com>
2019-09-24 14:26:38 -0600

    Add files via upload

---
+++ Looping Exercises/main.js
@@ -0,0 +1,28 @@
+// Exercise 1
+for(let i = 0; i < 500; i ++){
+    console.log("I'm so happy!");
+}
+
+// Exercise 2
+for(let i = 12; i<=800; i += 4){
+    console.log(i);
+}
+
+// Exercise 3
+for(let i = 55; i>=11; i -= 2){
+    console.log(i);
+}
+
+// Exercise 4
+let num = 0;
+for(let i = 5; i <= 50; i ++){
+    num += i;
+}
+console.log(num);
+
+// Exercise 5
+let num = 0;
+for(let i = 10; 1 <= 100; i += 10){
+    num += i;
+}
+console.log(num);
